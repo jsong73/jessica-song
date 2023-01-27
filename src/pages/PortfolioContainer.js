7e3b687468ce8730adf247e7c24f69dbbd474878
jsong73 <jessicajinsong@gmail.com>
2023-01-27 15:22:01 -0500

    added img

--- src/pages/PortfolioContainer.js
+++ src/pages/PortfolioContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Woman from "../images/woman.jpg"
 
 function PortfolioContainer () {
     return(
@@ -7,8 +7,9 @@ function PortfolioContainer () {
 
         <div 
             id="home" 
-            className="bg-red-200 snap-start w-screen h-screen"> 
-            <nav>
+            className="snap-start w-screen h-screen"> 
+
+            <nav className="">
                 <ul>
                     <li> <a href="#home">Home</a> </li>
                     <li> <a href="#about">About</a> </li>
@@ -17,19 +18,21 @@ function PortfolioContainer () {
                 </ul>
             </nav>
 
-            <h1 id="full-name" className="text-center justify-center items-center"> Jessica    
-            Song.    
+            <h1 id="full-name" className="text-8xl text-center justify-center 
+            items-center"> Jessica Song.
             </h1>
 
-            <h1 id="title" className="text-center justify-center items-center">Front-end developer 
-            / Back-end developer 
+            <h1 id="title" className=" text-3xl mt-8 text-center justify-center 
+            items-center"> — front-end developer & back-end developer —
             </h1>
 
-            <p id="bio" className="mt-8 text-center justify-center items-center"> A full stack 
-            web developer empowered by building highly granular code and delivering hyperscaling 
-            solutions.
+            <img className="justify-center items-center" src={Woman} width="500px" alt="woman- 
+            computer"/>
+
+            <p id="bio" className="text-xl mt-8 text-center justify-center items-center"> A 
+            full stack web developer empowered by <b>building</b> highly granular code and 
+            <b> delivering </b> hyperscaling solutions.
             </p>
-
 
         </div>
 
