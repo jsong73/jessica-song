12ff4eef148181197c7ed10f01c7a3f8f09303a5
jsong73 <jessicajinsong@gmail.com>
2023-01-30 15:10:59 -0500

    fixed mobile

--- src/pages/PortfolioContainer.js
+++ src/pages/PortfolioContainer.js
@@ -7,11 +7,11 @@ function PortfolioContainer () {
 
         <div 
             id="home" 
-            className="snap-start w-screen h-screen"> 
+            className="snap-start w-screen h-screen "> 
 
             <nav className="flex items-center justify-center flex-wrap p-6">
-                <div className="flex items-center flex-shrink-0 mr-6">
-                        <ul className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                <div className="flex-shrink-0 mr-6">
+                        <ul className="w-full block flex-grow lg:flex lg:items-center lg:h-auto">
                             <div className="text-md lg:flex-grow ">
                                 <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 01. <a href="#home">Home</a>
                                 </li>
@@ -26,26 +26,24 @@ function PortfolioContainer () {
                 </div>
             </nav>
 
-                <div className="px-6 py-12 h-full">
+                <div className="px-6">
                     <div className="flex justify-center items-center flex-wrap">
-                        <div className="md:w-1/2 lg:w-3/12">
+                        <div className="md:w-8/12 lg:w-4/12 lg:ml-20">
                         
-                    <img src={Woman} alt="woman-computer"/>
+                    <img className="h-auto max-w-full py-1" src={Woman} alt="woman-computer"/>
          
                         </div>
 
-
-                <div className=" md:w-4/8 lg:w-5/12 lg:ml-20">
-                    <h1 id="full-name" className="max-w-screen-lg mx-auto mt-20 flex text-8xl text-center justify-center items-center">JESSICA SONG.
+                <div className="md:w-4/8 lg:w-5/12 lg:ml-20">
+                    <h1 id="full-name" className="max-h-screen-lg text-center justify-center items-center mt-10 md:text-8xl sm: text-4xl">JESSICA SONG.
                     </h1>
+            
+                <p id="bio" className="md:text-xl mt-8 text-center justify-center items-center"> A full stack web developer empowered by <b className="italic text-blue-500"> building </b> highly granular code   
+                and <b className="italic text-blue-500"> delivering </b> hyperscaling solutions.
+                </p>
 
             
-            <p id="bio" className="text-xl mt-8 text-center justify-center items-center"> A 
-            full stack web developer empowered by <b className="italic text-blue-500"> building </b> highly granular code and 
-            <b className="italic text-blue-500"> delivering </b> hyperscaling solutions.
-            </p>
-
-            <div className="flex text-center justify-center items-center mt-8">
+                <div className="flex text-center justify-center items-center mt-8">
                 <a 
                     className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 
                     focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 
@@ -56,16 +54,11 @@ function PortfolioContainer () {
                     rel="noopener noreferrer"> Check out my resume
                 </a>
              </div>
-       
-
-            {/* <div className="hero container max-w-screen-lg mx-auto pb-10 flex justify-center">
-            <img src={Woman} width="530px" alt="woman- 
-            computer"/>
-            </div> */}
-                        </div>
+    
                     </div>
                 </div>
             </div>
+        </div>
      
 
 
