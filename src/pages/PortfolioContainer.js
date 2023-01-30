51f562411fd9c372c891d4e44d2bfed25b055f85
jsong73 <jessicajinsong@gmail.com>
2023-01-30 13:21:19 -0500

    img

--- src/pages/PortfolioContainer.js
+++ src/pages/PortfolioContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Woman from "../images/woman.jpg"
+import Woman from "../images/coding.png"
 
 function PortfolioContainer () {
     return(
@@ -13,33 +13,39 @@ function PortfolioContainer () {
                 <div className="flex items-center flex-shrink-0 mr-6">
                         <ul className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                             <div className="text-md lg:flex-grow ">
-                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 01. <a href="#home">Home</a></li>
-                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 02. <a href="#about">About</a></li>
-                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 03. <a href="#projects">Projects</a></li>
-                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 04. <a href="#contact">Contact</a></li>
+                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 01. <a href="#home">Home</a>
+                                </li>
+                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 02. <a href="#about">About</a>
+                                </li>
+                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 03. <a href="#projects">Projects</a>
+                                </li>
+                                <li className="mr-5 inline-block hover:text-blue-500 hover:italic"> 04. <a href="#contact">Contact</a>
+                                </li>
                             </div>
                         </ul>
                 </div>
             </nav>
 
-            {/* grey border */}
-            <div className="w-full border-b border-gray-300"></div>
+                <div className="px-6 py-12 h-full">
+                    <div className="flex justify-center items-center flex-wrap">
+                        <div className="md:w-1/2 lg:w-3/12">
+                        
+                    <img src={Woman} alt="woman-computer"/>
+         
+                        </div>
 
 
-            <h1 id="full-name" className="max-w-screen-lg mx-auto mt-10  flex
-            text-8xl text-center justify-center items-center"> Jessica Song.
-            </h1>
-
-            <h1 id="title" className="flex text-3xl mt-8 text-center justify-center items-center"> — front-end & back-end developer —
-            </h1>
+                <div className=" md:w-4/8 lg:w-5/12 lg:ml-20">
+                    <h1 id="full-name" className="max-w-screen-lg mx-auto mt-20 flex text-8xl text-center justify-center items-center">JESSICA SONG.
+                    </h1>
 
             
             <p id="bio" className="text-xl mt-8 text-center justify-center items-center"> A 
-            full stack web developer empowered by <b className="italic"> building</b> highly granular code and 
-            <b className="italic"> delivering </b> hyperscaling solutions.
+            full stack web developer empowered by <b className="italic text-blue-500"> building </b> highly granular code and 
+            <b className="italic text-blue-500"> delivering </b> hyperscaling solutions.
             </p>
 
-            <div className="flex text-center justify-center items-center mt-8 ">
+            <div className="flex text-center justify-center items-center mt-8">
                 <a 
                     className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 
                     focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 
@@ -47,21 +53,20 @@ function PortfolioContainer () {
                     href="https://docs.google.com/document/d/1KtbFgZj_dZ6uu4P9ycTLqT_DTbnaB5PlaWuuW88YAuc/edit" 
                     aria-label="email" 
                     target="_blank" 
-                    rel="noopener noreferrer"> View my resume
+                    rel="noopener noreferrer"> Check out my resume
                 </a>
              </div>
        
 
-{/* 
-            <div className="hero container max-w-screen-lg mx-auto pb-10 flex justify-center">
-            <img src={Woman} width="520px" alt="woman- 
+            {/* <div className="hero container max-w-screen-lg mx-auto pb-10 flex justify-center">
+            <img src={Woman} width="530px" alt="woman- 
             computer"/>
             </div> */}
-
-
-
-        </div>
-
+                        </div>
+                    </div>
+                </div>
+            </div>
+     
 
 
 
